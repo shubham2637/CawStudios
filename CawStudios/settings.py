1e6f07bf8374904433808b20c0797a87d7ff7bc4
shubham2637 <shubham37@outlook.com>
2021-10-17 22:45:09 +0530

    heroku deploy #7

--- CawStudios/settings.py
+++ CawStudios/settings.py
@@ -84,8 +84,11 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
